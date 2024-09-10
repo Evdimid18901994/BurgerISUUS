371453741ee11b508d34845a476fecc2416453ad
Evdimid18901994 <99124478+Evdimid18901994@users.noreply.github.com>
2024-09-10 12:51:43 +0300

    Finish
    gg

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BurgerISUUS
- 
+# ISUUS
---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("burger2").addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("open")
+})
+})
