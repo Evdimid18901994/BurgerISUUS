72fb858b656e60cb3b520e6dfef5bbb5787a88a7
Evdimid18901994 <99124478+Evdimid18901994@users.noreply.github.com>
2024-09-20 11:46:55 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+
 document.getElementById("burger2").addEventListener("click", function() {
     document.querySelector(".navbar").classList.toggle("open")
-})
 })
